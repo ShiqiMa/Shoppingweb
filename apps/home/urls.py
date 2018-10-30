10921116d48bd8e1282f35294213e836cdb60941
msq <mashiqinc@163.com>
2018-10-30 12:24:42 +0800

    small modify in product page

--- apps/home/urls.py
+++ apps/home/urls.py
@@ -4,5 +4,6 @@ from . import views
 app_name = 'home'
 
 urlpatterns = [
-    path('', views.product, name='product')
+    path('', views.product, name='product'),
+    path('<int:products_id>/', views.single_product, name='single_product')
 ]
--- apps/home/views.py
+++ apps/home/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from .models import Products, ProductCategory
 
 def index(request):
     return render(request, 'main/index.html')
@@ -6,3 +7,14 @@ def index(request):
 
 def product(request):
     return render(request, 'main/product_detail.html')
+
+
+def single_product(request, products_id):
+    try:
+        product = Products.objects.select_related('category').get(pk=products_id)
+        context = {
+            'product': product
+        }
+        return render(request, 'main/single_product.html', context=context)
+    except News.DoesNotExist:
+        raise Http404
